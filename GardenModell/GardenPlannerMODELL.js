211f177bcf287ef2da6a8a064627447853659edd
Okkerka <43291278+Okkerka@users.noreply.github.com>
2026-02-24 09:29:19 +0100

    gitignore added
    added gitignore for db.env (safer SQL connection management) + node_modules since npm install reads from package.json on what to install, less clutter in files.

--- GardenModell/GardenPlannerMODELL.js
+++ GardenModell/GardenPlannerMODELL.js
@@ -1,26 +1,20 @@
 const mysql = require("mysql2/promise")
-// const dotenv = require("dotenv")
 const connection = mysql.createPool({
-    // host: process.env.MSQL_HOST,
-    // user: process.env.MSQL_USER,
-    // password: process.env.MSQL_PASSWORD,
-    // database: process.env.MSQL_DATABASE
-
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
-    database: 'sproutified_db'
+   host: process.env.db.host,
+   user: process.env.db.user,
+   password: process.env.db.password,
+   database: process.env.db.database
 })
 
 module.exports = {
-    GetAllIdeas: async function GetAllIdeas() {
-        const [rows] = await connection.query(`SELECT * FROM ideas`)
-        return rows
-    },
-    GetAllKnowledges : async function GetAllKnowledges() {
-        const [rows] = await connection.query(`SELECT * FROM knowledges`)
-        return rows
-    },
+    //GetAllIdeas: async function GetAllIdeas() {
+    //    const [rows] = await connection.query(`SELECT * FROM ideas`)
+    //    return rows
+    //},
+    //GetAllKnowledges : async function GetAllKnowledges() {
+    //  const [rows] = await connection.query(`SELECT * FROM knowledges`)
+    //  return rows
+    //},
     // GetSearchedPlantDetails : async function GetSearchedPlantDetails(commonName,botanicalName,type,water,sunlight,soil,planting,harvesting) {
     //     const [rows] = await connection.query(`SELECT * FROM plants WHERE commonName = ? OR botanicalName = ? OR type = ? OR water = ? OR sunlight = ? OR soil = ? OR planting = ? OR harvesting = ?`, [commonName, botanicalName, type, water, sunlight, soil, planting, harvesting])
     //     console.log(rows[0])
