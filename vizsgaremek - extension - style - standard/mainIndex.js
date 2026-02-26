abcdf5828e8ebd083087d7ae54bb885f6f8a3c1c
ProgiG05 <szabo.gergo1005@gmail.com>
2026-02-26 22:59:56 +0100

    huge steps so far but more to go
    halo

--- vizsgaremek - extension - style - standard/mainIndex.js
+++ vizsgaremek - extension - style - standard/mainIndex.js
@@ -15,6 +15,9 @@ document.getElementById('closeSidePanel').addEventListener('click',() => {
     sidePanel.style.transitionBehavior = 'ease-in'
     sidePanel.style.left = '-360px'
 })
+document.getElementById('darkmode').addEventListener('click',() => {
+    darkBtn.innerHTML = `<img style="float: right; transition: all 1s ease;" src="./pics/light_mode_30dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.png" alt="">`
+})
 document.getElementById("gardenMakerPage_Btn").addEventListener("mouseover", () => {
     cube.style.animation = 'rotate 30s linear infinite'
 })
@@ -22,5 +25,16 @@ document.getElementById("gardenMakerPage_Btn").addEventListener("mouseleave", ()
     cube.style.animation = ''
 })
 
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+    })
+})
 
-
+const hiddenElements = document.querySelectorAll(".hidden")
+hiddenElements.forEach((e) => observer.observe(e))
