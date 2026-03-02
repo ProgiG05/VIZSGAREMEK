86a1312c1c28f5d66ab4b99429b90dfb4563d330
ProgiG05 <szabo.gergo1005@gmail.com>
2026-03-02 14:28:28 +0100

    progress
    h

--- vizsgaremek - extension - style - standard/index.js
+++ vizsgaremek - extension - style - standard/index.js
@@ -2,17 +2,30 @@ const settingsBtn = document.getElementById('settings_Btn');
 const sidePanel = document.getElementById('settings-sidepanel');
 const darkBtn = document.getElementById('darkmode');
 const cube = document.getElementById('cube');
+
+const ideasBtn = document.getElementById("goto_Ideas")
+const plantfinderBtn = document.getElementById("goto_PlantFinder")
+const knowledgesBtn = document.getElementById("goto_Knowledge")
+
+
+ideasBtn.addEventListener("click", () => {
+    window.scrollTo({top:750,behavior:"smooth"})
+})
+plantfinderBtn.addEventListener("click", () => {
+    window.scrollTo({top:1400,behavior:"smooth"})
+})
+knowledgesBtn.addEventListener("click"  , () => {
+    window.scrollTo({top:2250,behavior:"smooth"})
+})
 document.addEventListener('DOMContentLoaded', () => {
 
 });
 document.getElementById('settings_Btn').addEventListener('click',() => {
-    sidePanel.style.transitionDuration = '0.2'
-    sidePanel.style.transitionBehavior = 'ease-in'  
+    sidePanel.style.transition = '0.4s all ease'
     sidePanel.style.left = 0
 })
 document.getElementById('closeSidePanel').addEventListener('click',() => {
-    sidePanel.style.transitionDuration = '0.2'
-    sidePanel.style.transitionBehavior = 'ease-in'
+    sidePanel.style.transition = '0.4s all ease'
     sidePanel.style.left = '-360px'
 })
 
@@ -55,3 +68,12 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll(".hidden")
 hiddenElements.forEach((e) => observer.observe(e))
+
+document.querySelectorAll(".detailsListItem").forEach(item => {
+    item.addEventListener("click", () => {
+        item.classList.toggle("active")
+    })
+})
+function toggleSaveState(buttonElement) {
+    buttonElement.classList.toggle('saved');
+}
