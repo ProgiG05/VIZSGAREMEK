3fb7f496546717d3c4f7ecdc6e818b57a4ebc31f
Csabito <majnarcsaba12@gmail.com>
2026-03-03 12:16:32 +0100

    garden createTable (numbers only)

--- GardenPublic/mains/gardens.js
+++ GardenPublic/mains/gardens.js
@@ -2,16 +2,34 @@ document.addEventListener("DOMContentLoaded", async () => {
     const gardensContainer = document.getElementById("gardens-container")
     const response = await fetch("/api/gardens", {
         method: "GET",
-        headers: {"Content-Type": "application/json"}
+        headers: { "Content-Type": "application/json" }
     })
     const gardens = await response.json()
     console.log(gardens)
     gardens.forEach(garden => {
+        const splittedContent = garden.gardencontent.split(";")
         const gardenCard = document.createElement("div")
         gardenCard.innerHTML = `
-            <h2>${garden.name}</h2>
-            <p>${garden.size}</p>
+            <h2>${garden.gardenname}</h2>
+            <p>${garden.gardencontent}</p>
         `
+        gardenCard.appendChild(CreateTable(splittedContent))
         gardensContainer.appendChild(gardenCard)
     })
 })
+
+function CreateTable(splittedContent) {
+    const table = document.createElement("table")
+    table.className = "garden-table"
+    splittedContent.forEach(row => {
+        const columns = row.split(",")
+        const tableRow = document.createElement("tr")
+        columns.forEach(column => {
+            const tableColumn = document.createElement("td")
+            tableColumn.textContent = column
+            tableRow.appendChild(tableColumn)
+        })
+        table.appendChild(tableRow)
+    })
+    return table
+}
