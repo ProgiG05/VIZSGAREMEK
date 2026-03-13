cf789f9b05e7a6ce817b34b0b1f3407eb59650bb
ProgiG05 <szabo.gergo1005@gmail.com>
2026-03-13 12:42:25 +0100

    ideas page update
    halo

--- GardenPublic/mains/mainIndex.js
+++ GardenPublic/mains/mainIndex.js
@@ -68,7 +68,7 @@ document.getElementById("gardenMakerPage_Btn").addEventListener("mouseleave", ()
 
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry)
+        // console.log(entry)
         if (entry.isIntersecting) {entry.target.classList.add('show')} 
         else {entry.target.classList.remove('show')}
     })
